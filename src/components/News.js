989a4bbec61ba10fe22f11aa89caaa64a64f901a
VijayPatidarrr <122171185+VijayPatidarrr@users.noreply.github.com>
2024-06-01 00:25:00 +0530

    fixed

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
 import axios from "axios";
 
 export class News extends Component {
